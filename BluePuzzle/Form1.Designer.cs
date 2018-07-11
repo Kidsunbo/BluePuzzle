0266a070b3b61d3790cb8b71500ede076667c867
kidsunbo <qiongxiaozi158@sina.com>
2018-07-11 14:23:09 +0800

    First push to github

---
+++ BluePuzzle/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BluePuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainPannel = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label1.Location = new System.Drawing.Point(30, 30);
+            this.Label1.Margin = new System.Windows.Forms.Padding(30);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(1, 40);
+            this.Label1.TabIndex = 1;
+            // 
+            // Label2
+            // 
+            this.Label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label2.Location = new System.Drawing.Point(90, 30);
+            this.Label2.Margin = new System.Windows.Forms.Padding(30);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(1, 40);
+            this.Label2.TabIndex = 2;
+            // 
+            // Label3
+            // 
+            this.Label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label3.Location = new System.Drawing.Point(150, 30);
+            this.Label3.Margin = new System.Windows.Forms.Padding(30);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(453, 40);
+            this.Label3.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.Label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Label2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(633, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // mainPannel
+            // 
+            this.mainPannel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mainPannel.Location = new System.Drawing.Point(0, 101);
+            this.mainPannel.Name = "mainPannel";
+            this.mainPannel.Size = new System.Drawing.Size(605, 605);
+            this.mainPannel.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(633, 741);
+            this.Controls.Add(this.mainPannel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "BluePuzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel mainPannel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ BluePuzzle/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluePuzzle
+{
+    public partial class Form1 : Form
+    {
+        #region Panels
+        Point positionOfPanel;
+
+        // When the main Form has been resized, this function will adjust the mainPanel's position
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                this.mainPannel.Location = new Point((this.Size.Width - this.mainPannel.Width) / 2, this.positionOfPanel.Y);
+            }
+            else if (WindowState == FormWindowState.Normal)
+            {
+                this.mainPannel.Location = this.positionOfPanel;
+            }
+        }
+        #endregion
+
+        #region Labels
+        string contentOfLabel1 = "当前分数为：{0}";
+        string contentOfLabel2 = "当前步数为：{0}";
+        string contentOfLabel3 = "所有步数为：{0}";
+
+        #endregion
+
+
+        #region Buttons
+        Button[,] buttons;
+
+
+        // Create all the buttons
+        private void createAndLayButtons()
+        {
+            for(int row = 0;row < Level; row++)
+            {
+                for(int col = 0; col < Level; col++)
+                {
+                    var button = createButton();
+                    button.Tag = new Point{ Y = row, X = col };
+                    var point = new Point();
+                    point.Y = row * (button.Height + 1);
+                    point.X = col * (button.Width + 1);
+                    button.Location = point;
+                    button.MouseClick += Button_MouseClick;
+                    buttons[row,col] = button;   
+                }
+
+            }
+            foreach (var button in buttons)
+            {
+                this.mainPannel.Controls.Add(button);
+            }
+
+        }
+
+
+        private void Button_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            var button = sender as Button;
+            if (button == null) return;
+
+            this.ReverseColor(button);
+            var point = (Point)button.Tag;
+
+            bool[] URDL = { true, true, true, true }; // Up,Right,Down,Left
+            if (point.X == 0) URDL[3] = false;
+            if (point.X == Level - 1) URDL[1] = false;
+            if (point.Y == 0) URDL[0] = false;
+            if (point.Y == Level - 1) URDL[2] = false;
+
+            if (URDL[0]) this.ReverseColor(buttons[point.Y - 1, point.X]);
+            if (URDL[1]) this.ReverseColor(buttons[point.Y, point.X + 1]);
+            if (URDL[2]) this.ReverseColor(buttons[point.Y + 1, point.X]);
+            if (URDL[3]) this.ReverseColor(buttons[point.Y, point.X - 1]);
+
+            this.CurrentStep++;
+            this.TotalStep++;
+            Label2.Text=contentOfLabel2.Replace("{0}", this.CurrentStep.ToString());
+            Label3.Text = contentOfLabel3.Replace("{0}",this.TotalStep.ToString());
+
+            if (this.checkSuccess())
+            {
+                this.Level++;
+                this.Label1.Text = contentOfLabel1.Replace("{0}", this.Level.ToString());
+                this.CurrentStep = 0;
+                ResetButtons();
+            }
+        }
+
+        private void ResetButtons()
+        {
+            buttons = new Button[Level, Level];
+            this.mainPannel.Controls.Clear();
+            this.createAndLayButtons();
+        }
+
+        // Reverse the color
+        void ReverseColor(Button button)
+        {
+            if(button.BackColor == Color.Blue)
+            {
+                button.BackColor = Color.Orange;
+            }
+            else
+            {
+                button.BackColor = Color.Blue;
+            }
+
+        }
+
+        // Get a button with the given size.
+        private Button createButton()
+        {
+            
+            Button button = new Button();
+            button.BackColor = Color.Orange;
+            button.Height = (this.mainPannel.Height - Level + 1) / Level;
+            button.Width = (this.mainPannel.Width - Level + 1) / Level;
+            button.Margin = new Padding(0);
+            button.Padding = new Padding(0);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 1;
+
+            return button;
+
+        }
+
+
+        // Check if I win.
+        private bool checkSuccess()
+        {
+            foreach(var button in buttons)
+            {
+                if(button.BackColor == Color.Orange)
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+        #endregion
+
+        #region Data
+        int Level = 1;
+        int TotalStep = 0;
+        int CurrentStep = 0;
+
+        #endregion
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This function does some initial behaviors, such as adding some buttons.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            positionOfPanel = this.mainPannel.Location;
+            this.Label1.Text = contentOfLabel1.Replace("{0}", this.Level.ToString());
+            this.Label2.Text = contentOfLabel2.Replace("{0}", this.CurrentStep.ToString());
+            this.Label3.Text = contentOfLabel3.Replace("{0}", this.TotalStep.ToString());
+            buttons = new Button[Level, Level];
+            this.createAndLayButtons();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.R)
+            {
+                this.mainPannel.Controls.Clear();
+                this.createAndLayButtons();
+                this.TotalStep -= this.CurrentStep;
+                this.CurrentStep = 0;
+                Label2.Text = contentOfLabel2.Replace("{0}", this.CurrentStep.ToString());
+                Label3.Text = contentOfLabel3.Replace("{0}", this.TotalStep.ToString());
+
+            }
+        }
+    }
+}
